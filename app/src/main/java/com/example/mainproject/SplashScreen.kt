35e4450268706da1cdc9dcba74560e93d5b60ac5
Afraim1123 <afraimahmedkhan@gmail.com>
2026-07-08 00:54:51 +0600

    Splash screen, log in page done

---
+++ app/src/main/java/com/example/mainproject/SplashScreen.kt
@@ -0,0 +1,30 @@
+package com.example.mainproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mainproject.databinding.ActivityLogInBinding
+import com.example.mainproject.databinding.ActivitySplashScreenBinding
+import java.util.logging.Handler
+
+class SplashScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding= ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LogIn::class.java))
+        },3000)
+    }
+}
